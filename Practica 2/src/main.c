4336de3895a0f399715743450f3239ff0a4310b9
MiaguawU <isis.miaguaw@gamil.com>
2026-09-20 00:17:38 -0600

    Version 2 practica 2, funcional con txt y consola

--- Practica 2/src/main.c
+++ Practica 2/src/main.c
@@ -1,5 +1,15 @@
 #include "main.h"
 
+/*
+gcc -c archivo.c
+gcc *.o -o pr2
+.\pr2
+
+CON TXT:
+crear archivo entrada.txt poner datos
+
+gcc pr2.c -o pr2; .\pr2; code resultado.txt
+*/
 int main(){
     if(!ctr_perm()){
         printf("ERROR");
--- Practica 2/src/permutacion.c
+++ Practica 2/src/permutacion.c
@@ -19,13 +19,15 @@
         A,D,B,C
 */
 #include "main.h"
-
-int factorial(int n){
+bool arr_fact(int arr[],int n){
     int facto=1;
-    for(int i=n; i>0;i--){
+    int cont=0;
+    for(int i=2; i<=n;i++){
         facto*=i;
+        arr[cont]=facto;
+        cont++;
     }
-    return facto;
+    return true;
 }
 bool swap(char cadena[], int a, int b){
     char aux= cadena[a];
@@ -33,29 +35,48 @@ bool swap(char cadena[], int a, int b){
     cadena[b]=aux;
     return true;
 }
-int permutar(char cadena[], int n){
+int permutar(char cadena[], int n, int factoriales[]){
     int i, j;
     int cont=1;
     printf("\n\n\n");
     printf("%d-%s\n",cont,cadena);
     bool intercambio=false;
-    for(i=1;i<factorial(n) ;i++){
+
+    bool cambia= false;
+    for(i=1;i<factoriales[n-2] ;i++){
         cont++;
         for(j=n-1; j>1; j--){
-            if((i) %  factorial(j)==0){ 
-                if(i ==18 || (i-18)%24==0){//intercambiar en funcion de factorial n con el ultimo
+            if((i) %  factoriales[j-2]==0){ //pienso que el siguiente if en impares apartir del 3
+                if(j%2!=0){//intercambiar en funcion de factorial n con el ultimo
+
+                    for(int k=3; k<=j; k++){//si es 5, seria 5 y 4
+                        if(i ==(k)*factoriales[j-2 ] || 
+                            (i-(k)*factoriales[j-2 ])% factoriales[j-2 + 1]==0){
+                                if(!swap(cadena, n- (k-1), n-1-j)){
+                                    return ERROR;
+                                }
+                                cambia=true;
+                        }
+                    }
+                    
+                }
+                /*else if(j%2!=0&&
+                    (i ==(3)*factoriales[j-2 ] || 
+                        (i-(3)*factoriales[j-2 ])% factoriales[j-2 + 1]==0)){//intercambiar en funcion de factorial n con el ultimo
                     //n-j=3 o j%3 si es 4 da 1 si 5 da 2
                     if(!swap(cadena, n-2, n-1-j)){
                         return ERROR;
                     }
-                }
-                else{
+                    cambia=true;
+                }*/
+                if(!cambia){
                     if(!swap(cadena, n-1, n-1-j)){
                         return ERROR;
                     }
                 }
                 printf("%d-%s\n",cont,cadena);
                 intercambio=true;
+                cambia=false;
                 break;
             }
         }
@@ -69,9 +90,9 @@ int permutar(char cadena[], int n){
         intercambio=false;
         
     }
-    return factorial(n);
+    return factoriales[n-2];
 } 
-bool ctr_perm(){
+bool ctr_perm_consola(){
     int len;
     printf("Introdusca la longitud\n");
     scanf("%d", &len);
@@ -87,12 +108,66 @@ bool ctr_perm(){
     fgets(cadena,len+1,stdin);
     *(cadena + strcspn(cadena, "\n")) = '\0';
 
-    if(permutar(cadena, len)==ERROR){
+    int factoriales[len - 1];
+    
+    if (!arr_fact(factoriales, len)) {
+        printf("ERROR: no se pudo obtener el factorial\n");
+        return false;
+    }
+
+    if(permutar(cadena, len, factoriales)==ERROR){
         printf("ERROR al permutar\n");
         return false;
     }
 
     printf("\n");
+    return true;
+}
+bool ctr_perm(){
+    freopen("entrada.txt", "r", stdin);   
+    freopen("resultado.txt", "w", stdout); 
+
+    if (stdin == NULL || stdout == NULL) {
+        printf("Error al abrir los archivos\n");
+        return 1;
+    }
+
+    char buffer[50];
+    int len;
+
+    printf("Introdusca la longitud\n");
+    if (fgets(buffer, sizeof(buffer), stdin) != NULL) {
+        len = atoi(buffer);
+    } else {
+        printf("Error al leer la longitud\n");
+        return 1;
+    }
+
+    if (len < 2 || len > 15) {
+        printf("ERROR: no se puede generar permutaciones con esa longitud\n");
+        return false;
+    }
+
+    char cadena[len + 1];
+    int factoriales[len - 1];
+    
+    if (!arr_fact(factoriales, len)) {
+        printf("ERROR: no se pudo obtener el factorial\n");
+        return false;
+    }
+
+    printf("Intrusca la cadena\n");
+    fgets(cadena, len + 1, stdin);
+    *(cadena + strcspn(cadena, "\n")) = '\0';
+
+    if(permutar(cadena, len, factoriales)==ERROR){
+        printf("ERROR al permutar\n");
+        return false;
+    }
+
+    printf("\n");
+    fclose(stdin);
+    fclose(stdout);
     return true;
 }
 
